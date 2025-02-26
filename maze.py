bc6b004d3cd13ba6b4f29cf0289c3c61b5dc0d76
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-02-25 23:26:16 -0800

    add maze area

---
+++ maze.py
@@ -0,0 +1,52 @@
+import time
+
+from cell import Cell
+
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+
+        self._create_cells()
+
+    def _create_cells(self):
+        # add cells
+        for i in range(self._num_cols):
+            cols = []
+            for j in range(self._num_rows):
+                cols.append(Cell(self._win))
+            self._cells.append(cols)
+
+        # draw cells
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        if self._win is None:
+            return
+
+        # calculate cell size for drawing
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+
+        # execute draw and animation
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        if self._win is None:
+            return
+
+        # redraw window and delay
+        self._win.redraw()
+        time.sleep(0.05)
